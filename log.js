90615f050d1b766cf77be69b40335b6d1a1b46a9
nino zhang <ninohit@gmail.com>
2012-10-16 19:54:27 +0800

    更新

---
+++ log.js
@@ -0,0 +1,30 @@
+/**
+ * log.js
+ */
+(function() {
+
+    function $() {
+
+    }
+
+    function css(key, value) {
+
+    }
+
+    var Log = function() {
+
+    };
+
+    Log.prototype.defaults = {
+
+    };
+
+    Log.prototype.config = function() {
+
+    };
+    
+    Log.prototype.append = function() {
+
+    };
+
+})();
